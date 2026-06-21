34aed3a8ff67ba68174c0489e627ce18d902ace4
任懋朋 <232602448+paddy20061219@users.noreply.github.com>
2026-06-21 13:51:58 +0800

    Add files via upload

---
+++ leetcode 70/leetcode70_wrong.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// LeetCode 70 - Climbing Stairs
+// wrong version: this code is intentionally very wrong.
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        // 蠢錯誤：直接回傳 n
+        // 題目要算方法數，不是樓梯有幾階就回傳幾。
+        return n;
+    }
+};
